9491ce0108392dbd3e7622bc5eca37a8dfe70951
farshadjanmohammdi@gmail.com <farshad.janmohammadi@gmail.com>
2026-02-14 21:33:18 +0330

    add icons pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,26 @@ export default function RootLayout({
         {/* <link rel="shortcut icon" href="/icon.webp" type="image/x-icon" /> */}
       </head>
       <body className={`${poppins.variable} ${outfit.variable} antialiased`}>
+        <head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="%PUBLIC_URL%/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="%PUBLIC_URL%/favicon.svg"
+          />
+          <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="%PUBLIC_URL%/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="%PUBLIC_URL%/site.webmanifest" />
+        </head>
         <Header />
         <QueryClientProvider client={queryClient}>
           {children}
